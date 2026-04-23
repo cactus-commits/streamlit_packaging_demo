70bc072a29c896e3dc24344305441156995237b3
cactus-commits <aurora.dev@gmai.com>
2026-04-23 23:47:53 +0200

    chore: set up components

--- salaries/src/salaries/components/kpi.py
+++ salaries/src/salaries/components/kpi.py
@@ -1,14 +1,26 @@
 
 #%% #skapar jupiter notebook
+# %%
 from salaries.utils.helpers import get_salaries_df
-import duckdb 
+import duckdb
+import streamlit as st
 
 df = get_salaries_df()
 
-avg_salary = duckdb.sql("""
-    FROM df 
+def avg_salary_usd_kpi(role, label):
+    avg_salary = (
+        duckdb.sql(f"""--sql
+        SELECT 
+            ROUND(AVG(salary_in_usd),-3)::INT AS avg_salary_usd
+        FROM df
+        WHERE job_title ILIKE '{role}'
+    """)
+        .df()
+        .iloc[0]
+    )
 
+    st.metric(label = label, value=avg_salary["avg_salary_usd"])
 
-""").df()
+# %%
 
 # %%
